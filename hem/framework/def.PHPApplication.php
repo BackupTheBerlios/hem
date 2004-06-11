acc160ef86eda19c395d96048a3ab61d230d0eab
mloitzl <mloitzl>
2004-06-11 13:38:55 +0000

    Added a minimal Class Loader for the Framework Class

---
+++ hem/framework/def.PHPApplication.php
@@ -0,0 +1,50 @@
+<?php
+
+$DEBUGGER_CLASS = 'classes/class.Debugger.php';
+$DBI_CLASS = 'classes/class.DBI.php';
+$HANDLER_CLASS = 'classes/class.Handler.php';
+$MESSAGE_HANDLER_CLASS = 'classes/class.MessageHandler.php';
+$ERROR_HANDLER_CLASS = 'classes/class.ErrorHandler.php';
+$LABEL_HANDLER_CLASS = 'classes/class.LabelHandler.php';
+$FORM_VALIDATOR_CLASS = 'classes/class.FormValidator.php';
+
+
+
+
+if(!defined("DEBUGGER_LOADED") && !empty($DEBUGGER_CLASS))
+  {
+    include_once $DEBUGGER_CLASS;
+  }
+
+if(!defined("DBI_LOADED") && !empty($DBI_CLASS))
+  {
+    include_once $DBI_CLASS;
+  }
+
+if(!defined("HANDLER_LOADED") && !empty($HANDLER_CLASS))
+  {
+    include_once $HANDLER_CLASS;
+  }
+
+if(!defined("MESSAGE_HANDLER_LOADED") && !empty($MESSAGE_HANDLER_CLASS))
+  {
+    include_once $MESSAGE_HANDLER_CLASS;
+  }
+
+if(!defined("ERROR_HANDLER_LOADED") && !empty($ERROR_HANDLER_CLASS))
+  {
+    include_once $ERROR_HANDLER_CLASS;
+  }
+
+if(!defined("LABEL_HANDLER_LOADED") && !empty($LABEL_HANDLER_CLASS))
+  {
+    include_once $LABEL_HANDLER_CLASS;
+  }
+
+if(!defined("FORM_VALIDATOR_LOADED") && !empty($FORM_VALIDATOR_CLASS))
+  {
+    include_once $FORM_VALIDATOR_CLASS;
+  }
+
+
+?>
