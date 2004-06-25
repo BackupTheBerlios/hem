a6bd1d56ebf3513a9d80ef4ed7340d769ad7c707
mloitzl <mloitzl>
2004-06-25 09:19:22 +0000

    Added label and messages file for sample app

---
+++ hem/framework/labels.sampleApp.php
@@ -0,0 +1,11 @@
+<?php
+
+$LABELS['US']['LOGOUT_BUTTON'] = 'Logout';
+$LABELS['US']['CANCEL_BUTTON'] = 'Cancel';
+
+
+$LABELS['DE']['LOGOUT_BUTTON'] = 'Abmelden';
+$LABELS['DE']['CANCEL_BUTTON'] = 'Abbrechen';
+
+
+?>
---
+++ hem/framework/messages.sampleApp.php
@@ -0,0 +1,10 @@
+<?php
+
+$MESSAGES['US']['AUTH_AS'] = 'You\'re authenticated as ';
+$MESSAGES['US']['SOME_MSG'] = 'I\'m doing something'; 
+
+$MESSAGES['DE']['AUTH_AS'] = 'Sie sind angemeldet als ';
+$MESSAGES['DE']['SOME_MSG'] = 'Ich mach etwas'; 
+
+
+?>
