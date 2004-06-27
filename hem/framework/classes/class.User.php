6ad6901e6c596a7617c2b94ec53471588dc72611
mloitzl <mloitzl>
2004-06-27 22:25:40 +0000

    Added User class, handles user related database stuff'

---
+++ hem/framework/classes/class.User.php
@@ -0,0 +1,58 @@
+<?php
+define('USER_CLASS_LOADED', TRUE);
+
+class User
+{
+  function User($uid)
+  {
+
+    // setup User
+    // TODO: setUserName, email,...
+
+    // TODO: set Users Theme
+
+
+
+  }
+
+
+  function getTheme()
+  {
+
+
+  }
+
+  function setTheme()
+  {
+
+  }
+
+  function getEmail()
+  {
+
+
+  }
+
+  function setEmail()
+  {
+
+  }
+
+
+  function getUserName()
+  {
+
+  }
+
+
+  function getRealName()
+  {
+
+  }
+
+
+  
+}
+
+
+?>
