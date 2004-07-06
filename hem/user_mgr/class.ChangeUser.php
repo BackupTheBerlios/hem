c1032f25f3e63dd48cd958aae2da5b3d70328490
mloitzl <mloitzl>
2004-07-06 13:04:19 +0000

    Started Usermanager Application

---
+++ hem/user_mgr/class.ChangeUser.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+class ChangeUser extends PHPApplication
+
+{
+
+  function ChangeUser($params)
+  {
+    PHPApplication::PHPApplication($params);
+  }
+
+
+
+
+
+
+}
+
+
+
+?>
---
+++ hem/user_mgr/conf.ChangeUser.php
@@ -0,0 +1,27 @@
+<?php
+require_once('../conf/global_conf.php');
+
+$PHP_SELF = $_SERVER['PHP_SELF'];
+
+$CHANGE_USER_TEMPLATE ='change_user.html';
+
+$APPLICATION_NAME = 'Change User';
+$APP_DIR = '/user_mgr';
+
+$REL_APP_PATH = $REL_APP_ROOT . $APP_DIR;
+
+$TEMPLATE_DIR = $APP_ROOT . $APP_DIR;
+$REL_TEMPLATE_DIR = $REL_APP_ROOT . $APP_DIR;
+// TODO: check --> $REL_TEMPLATE_DIR = $USER_DIR . $PROJECT_NAME . '/login';
+
+
+require_once 'class.PHPApplication.php';
+require_once 'class.ChangeUser.php';
+
+define('ERROR_FILE', 'errors.ChangeUser.php');
+define('MESSAGE_FILE', 'messages.ChangeUser.php');
+define('LABEL_FILE', 'labels.ChangeUser.php');
+
+
+
+?>
