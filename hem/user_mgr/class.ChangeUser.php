369aa5b181bece5a510d5d31c984afce502abc95
mloitzl <mloitzl>
2004-07-06 14:16:44 +0000

    Added change User app

--- hem/user_mgr/class.ChangeUser.php
+++ hem/user_mgr/class.ChangeUser.php
@@ -12,8 +12,114 @@ class ChangeUser extends PHPApplication
   }
 
 
+  function run()
+  {
+    $this->changeUserDriver();
+
+  }
+
+  function changeUserDriver()
+  {
+
+    if($this->getPostRequestField('step', null) == '1')
+      {
+	$this->changeUserData();
+      }
+    else 
+      {
+	$this->changeForm();
+      }
+    
+
+  }
 
 
+  function changeForm()
+  {
+    global $CHANGE_USER_TEMPLATE;
+
+    $this->showScreen($CHANGE_USER_TEMPLATE, 'displayChangeForm', $this->getAppName());
+  }
+
+  function changeUserData()
+  {
+
+    if($this->user_->updateUserData($this->getPostRequestField('data', null)))
+      {
+	// TODO: Drop a message, that everything was fine
+	if($this->getPostRequestField('url', null))
+	  {
+	    $this->debug($this->getPostRequestField('url', null));
+	    header("Location: ".$this->getPostRequestField('url', null)."");
+	  }
+	    else
+	  echo "now what?!?!";
+      }
+
+    else
+      {
+	// TODO: Drop a message, that something went wrong
+	print_r($this->getPostRequestField('data', null));
+      }
+    
+
+  }
+
+  function displayChangeForm(& $tpl)
+  {
+    global $PHP_SELF;
+
+    $this->debug("Display Change Form");
+
+    $tpl->setCurrentBlock('main_block');
+
+    $tpl->setVar(array(
+		       'SELF_PATH' => $PHP_SELF,
+		       'REDIRECT_URL' => $PHP_SELF
+		       )
+		 );
+    $tpl->setVar(array(
+		       'LABEL_FIRSTNAME' => $this->getLabelText('FIRST_NAME'),
+		       'LABEL_LASTNAME' => $this->getLabelText('LAST_NAME'),
+		       'LABEL_EMAIL' => $this->getLabelText('EMAIL'),
+		       'LABEL_STREET' => $this->getLabelText('STREET'),
+		       'LABEL_NO' => $this->getLabelText('NO'),
+		       'LABEL_CITY' => $this->getLabelText('CITY'),
+		       'LABEL_ZIP' => $this->getLabelText('ZIP'),
+		       'LABEL_COUNTRY' => $this->getLabelText('COUNTRY'),
+		       'LABEL_PHONE' => $this->getLabelText('PHONE'),
+		       'LABEL_COMMENT' => $this->getLabelText('COMMENT')
+		       )
+		 );
+    $tpl->setVar(array(
+		       'SUBMIT_BUTTON' => $this->getLabelText('SUBMIT_BUTTON'),
+		       'CANCEL_BUTTON' => $this->getLabelText('CANCEL_BUTTON')
+		       )
+		 );
+
+    $tpl->setVar(array(
+		       'FIRST_NAME' => $this->user_->first_name,
+		       'LAST_NAME' => $this->user_->last_name,
+		       'EMAIL' => $this->user_->email,
+		       'STREET' => $this->user_->street,
+		       'NO' => $this->user_->no,
+		       'CITY' => $this->user_->city,
+		       'ZIP' => $this->user_->zip,
+		       'COUNTRY' => $this->user_->country,
+		       'PHONE' => $this->user_->phone,
+		       'COMMENT' => $this->user_->comment,
+		       'USER_ID' => $this->user_->user_id_
+		       )
+		 );
+
+
+
+
+
+    $tpl->parseCurrentBlock();
+
+    return 1;
+  }
 
 
 }
--- hem/user_mgr/conf.ChangeUser.php
+++ hem/user_mgr/conf.ChangeUser.php
@@ -14,6 +14,8 @@ $TEMPLATE_DIR = $APP_ROOT . $APP_DIR;
 $REL_TEMPLATE_DIR = $REL_APP_ROOT . $APP_DIR;
 // TODO: check --> $REL_TEMPLATE_DIR = $USER_DIR . $PROJECT_NAME . '/login';
 
+// TODO: simplify!
+$APP_DSN = $AUTH_DB_URL;
 
 require_once 'class.PHPApplication.php';
 require_once 'class.ChangeUser.php';
